742451d0cb76c72274299c370ca82f4d7a7cb425
Svetlana <bel-svetik@yandex.ru>
2024-08-25 23:16:40 +0300

    Модуль 17, ДЗ №1

---
+++ HW1/app/routers/task.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from schemas import CreateTask
+
+router_task = APIRouter(prefix='/task', tags=['task'])
+
+
+@router_task.get('/')
+async def all_tasks():
+    pass
+
+
+@router_task.get('/task_id')
+async def task_by_id(task_id: int) -> CreateTask:
+    pass
+
+
+@router_task.post('/create')
+async def create_task():
+    pass
+
+
+@router_task.put('/update')
+async def update_task():
+    pass
+
+
+@router_task.delete('/delete')
+async def delete_task():
+    pass
